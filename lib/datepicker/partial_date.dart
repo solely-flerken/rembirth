4d465c49cae39d905b84cd6fdfa68c610111d794
solely-flerken <solely.flerken@proton.me>
2025-06-07 20:49:12 +0200

    ✨ string format for PartialDate

--- lib/datepicker/partial_date.dart
+++ lib/datepicker/partial_date.dart
@@ -4,6 +4,19 @@ class PartialDate {
   final int day;
 
   PartialDate({this.year, required this.month, required this.day});
+
+  String format() {
+    final monthStr = month.toString().padLeft(2, '0');
+    final dayStr = day.toString().padLeft(2, '0');
+    if (year != null) {
+      return '$year-$monthStr-$dayStr';
+    } else {
+      return '$monthStr-$dayStr';
+    }
+  }
+
+  @override
+  String toString() => format();
 }
 
 extension PartialDateConversion on DateTime {
